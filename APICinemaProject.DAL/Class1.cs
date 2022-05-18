577ae3841b391f3b8af73bce9fa2092d91b01165
AdamWintherAkqa <100200760+AdamWintherAkqa@users.noreply.github.com>
2022-05-18 10:54:30 +0200

    DAL added

---
+++ APICinemaProject.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace APICinemaProject.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ APICinemaProject.DAL/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICinemaProject.DAL.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public bool CustomerIsVIP { get; set; }
+        public string CustomerEmail { get; set; }
+        public bool CustomerGender { get; set; }
+        public string CustomerPassword { get; set; }
+
+    }
+}
