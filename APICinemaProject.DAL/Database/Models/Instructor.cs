433c0af9810eca4f32119aac93b635f69d7366b3
AdamWintherAkqa <100200760+AdamWintherAkqa@users.noreply.github.com>
2022-05-19 14:37:13 +0200

    deleted template controller

--- APICinemaProject.DAL/Database/Models/Instructor.cs
+++ APICinemaProject.DAL/Database/Models/Instructor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace APICinemaProject.DAL.Database.Models
 {
     public class Instructor
     {
+        [Key]
         public int InstructorID { get; set; }
         public string InstructorName { get; set; }
     }
